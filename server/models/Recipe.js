f5cc1de84c7bcf8554f2d26c622f5eb47ad77e7f
Viktorov616 <viktorov616@gmail.com>
2018-02-10 12:31:41 +0300

    Added slug and created date before saving recipe

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+const slug     = require('slugs');
 
 mongoose.Promise = global.Promise;
 
 const recipeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: 'You muse supply a name!',
+  created: {
+    type: Date,
+    default: Date.now,
   },
+  description: String,
   ingredients: [{
     name: {
       type: String,
@@ -14,9 +16,30 @@ const recipeSchema = new mongoose.Schema({
     },
     amount: String,
   }],
-  tags: [String],
-  description: String,
+  name: {
+    type: String,
+    required: 'You muse supply a name!',
+  },
   rating: Number,
+  slug: String,
+  tags: [String],
+});
+
+// create slug before saving recipe
+// if the recipe with this slug already exist, add an increment to it
+recipeSchema.pre('save', async function(next) {
+  if (!this.isModified('name')) return next();
+
+  this.slug = slug(this.name);
+
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`);
+  const recipesWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+  if (recipesWithSlug.length) {
+    this.slug = `${this.slug}-${recipesWithSlug.length + 1}`;
+  }
+
+  return next();
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
